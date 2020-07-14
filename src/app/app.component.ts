d57f0bdc76a7398783b33b2901151f151c1ce789
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-07-13 21:34:57 -0400

    assignment#3 - list of numbers added dynamically

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+    showText = false;
+    btnClics = [];
+    styleColor = '';
+    showDetails() {
+        this.showText = !this.showText;
+        this.btnClics.push(this.btnClics.length + 1);
+    }
+}
